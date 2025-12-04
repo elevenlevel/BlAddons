466bc7979bc4cc96e72b8adf2e789a80841aaea8
Roman Askarov <elevenlevel@gmail.com>
2025-12-04 13:46:05 +0500

    bakers

--- Extensions/XD_Baker/utils/bakers.py
+++ Extensions/XD_Baker/utils/bakers.py
@@ -5,14 +5,14 @@ from .functions import *
 
 def bake_diffuse_map(context, map_name):
     cage_object_shortcut = context.scene.xd_baker.temp_things.bake_cg_obj
-    cage_object = bpy.data.objects[cage_object_shortcut.name]
+    cage_obj = bpy.data.objects[cage_object_shortcut.name]
     
     if get_options_var(context, map_name, "Albedo"): 
         p_filter = set({'COLOR'})
     else:
         p_filter = set({'DIRECT', 'INDIRECT', 'COLOR'})
     
-    # bpy.ops.object.bake(type="DIFFUSE", use_selected_to_active=True, use_cage=True, cage_object=cage_object, margin=0, pass_filter=p_filter)
+    bpy.ops.object.bake(type="DIFFUSE", use_selected_to_active=True, use_cage=True, cage_object=cage_obj, margin=0, pass_filter=p_filter)
 
 def bake_ao_map(context, map_name):
     cage_object = context.scene.xd_baker.temp_things.bake_cg_obj
