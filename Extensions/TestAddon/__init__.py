c4fd82fd05bb45d047e72db8d4154647288834c9
Roman Askarov <elevenlevel@gmail.com>
2026-02-12 09:04:16 +0500

    Test Addon

--- Extensions/TestAddon/__init__.py
+++ Extensions/TestAddon/__init__.py
@@ -1,11 +1,26 @@
-from .additional import *
-from .addon_data import *
+import bpy
+
+class XD_PT_Panel(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_label = "Objects"
+    bl_category = "TEST"
+    
+    @classmethod
+    def poll(cls, context):
+        return True
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.label(text="Hello World!")
 
 def register():
-    print(cl.text)
+    bpy.utils.register_class(XD_PT_Panel)
+
 
 def unregister():
-    pass
+    bpy.utils.unregister_class(XD_PT_Panel)
 
 if __name__ == "__main__":
     register()
--- Extensions/TestAddon/utils/additional.py
+++ Extensions/TestAddon/utils/additional.py
@@ -2,6 +2,3 @@ from .addon_data import *
 
 class cl():
     text = attributes.info
-    def __init__(self):
-        print("additional hello")
-        
